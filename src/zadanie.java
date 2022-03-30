60a2a7c2e42994539867cd5c7ffebd0016eb9224
PAdam2 <ap008adam@gmail.com>
2022-03-30 14:38:54 +0200

    jakies rzeczy

---
+++ src/zadanie.java
@@ -0,0 +1,24 @@
+public class zadanie {
+    public static void main(String[] args) {
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque finibus lectus eu posuere " +
+                "aliquet. Nunc eleifend felis vehicula, bibendum mi a, maximus mi. Etiam ipsum enim, efficitur at urna" +
+                "ac, commodo commodo elit. Ut sit amet laoreet ligula. Nunc vitae commodo justo, sed sagittis velit." +
+                " Vestibulum fermentum varius nulla non aliquam. Vivamus euismod congue eros et blandit. Fusce euismod " +
+                "lacinia enim, vitae euismod metus hendrerit tincidunt. Cras facilisis gravida mi vel luctus. Nullam " +
+                "fermentum, odio vitae sodales rhoncus, ligula ligula vulputate turpis, et ornare enim sem sit amet quam." +
+                " Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum vehicula " +
+                "non velit et accumsan. Ut diam leo, molestie ut viverra cursus, bibendum sed lorem. Suspendisse potenti. Aenean " +
+                "interdum eleifend nisl, porttitor rutrum neque euismod ac. Nunc dolor tortor, molestie ut finibus sit amet, iaculis in nisl." +
+                "Ut pretium laoreet risus. Proin ornare erat a leo pellentesque ultricies. Ut vulputate sapien eget dictum vestibulum." +
+                " Duis ac diam ac turpis varius ornare. Pellentesque tempor vitae turpis eget pharetra. Morbi ac tempus mauris. Aliquam neque" +
+                " odio, dapibus ac orci eget, facilisis porttitor turpis. Integer et mi tortor. Donec interdum mollis nibh a molestie. Proin auctor enim nec lobortis.";
+        //char[] wartosc =  text.toCharArray();
+
+        System.out.println("dlugsoc tekstu");
+        System.out.println(text.length());
+        System.out.println("ilosc zankow");
+        System.out.println(text.contains(" "));
+        //System.out.println((int)wartosc[0]);
+
+    }
+}
---
+++ src/zadanie1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class zadanie1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Liczba n:  ");
+        int n = scan.nextInt();
+        System.out.println(n);
+
+    }
+
+
+
+}
